ec30d32a9fda3a7a75a4e42a53a4072a7abea412
w_cossey <willem.cossey@student.kuleuven.be>
2021-10-28 16:58:49 +0200

    Add type hints and abstract method decorator

--- src/helper/Distribution.py
+++ src/helper/Distribution.py
@@ -3,29 +3,34 @@ from numpy import random
 
 
 class Distribution(ABC):
+    @abstractmethod
     def __init__(self):
         pass
 
     @abstractmethod
-    def sample(self, amount: int) -> list[float]:
+    def sample(self, amount: int = 1) -> list[float]:
         pass
 
 
 class Uniform(Distribution):
-    def __init__(self, lower=0, upper=1):
+    def __init__(self, lower: int = 0, upper: int = 1):
         Distribution.__init__(self)
+        assert upper >= lower
         self.lower = lower
         self.upper = upper
 
-    def sample(self, amount=1):
+    def sample(self, amount: int = 1) -> list[float]:
         return list(random.uniform(self.lower, self.upper, amount))
 
 
 class Normal(Distribution):
     def __init__(self, mean=0, std=1):
         Distribution.__init__(self)
+        assert isinstance(mean, (float, int))
+        assert isinstance(std, (float, int))
+        assert std > 0
         self.mean = mean
         self.std = std
 
-    def sample(self, amount=1):
-        return random.normal(self.mean, self.std, amount)
+    def sample(self, amount: int = 1) -> list[float]:
+        return list(random.normal(self.mean, self.std, amount))
