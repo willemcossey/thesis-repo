2cdc78697b722ef51bc3add0d83e028113244689
w_cossey <willem.cossey@student.kuleuven.be>
2021-11-10 11:36:55 +0100

    Pivot to generating figure P&T p. 241 (opinion modelling) instead of fig 6.2 (choice formation) + Use matplotlib for plotting

--- src/generate-ims-figures.py
+++ src/generate-ims-figures.py
@@ -1,35 +1,76 @@
 from helper.SimulationJob import SimulationJob
-from helper.Distribution import Normal
-from math import sqrt
+from math import exp
 import pandas as pd
-import plotly.express as px
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
-# figure 6.2
+matplotlib.use("qt5agg")
 
-lamb = 0.003
-gamma = 0.1
-theta_std = sqrt(gamma * lamb)
-print(theta_std)
+# case: P = 1, D = 1-w^2
+
+lamb = 0.1
+theta_std = 0.1
+gamma = (theta_std ** 2) / lamb
+print(f"gamma= {gamma}")
+print(f"theta_std = {theta_std}")
 
 w_ = [-0.3, 0.7]
 rho = 0.5
 
-exponent = 2
+exponent = 1
+
+# compute reference solution as described p. 241
+
+
+def inv_dist(w, m, lam):
+    res = (1 + w) ** (-2 + (m / (2 * lam)))
+    res = res * (1 - w) ** (-2 - (m / (2 * lam)))
+    res = res * exp(-((1 - m * w) / (lam * (1 - w ** 2))))
+    return res
+
+
+resolution = 1000
+
+x = np.linspace(-0.99, 1, num=resolution, endpoint=False)
+y = [inv_dist(s, 0, lamb) for s in x]
+data = [x, y]
+data = np.transpose(data)
+reference = pd.DataFrame(data=data, columns=["w", "g_inf(w)"])
+
+# run MC simulation
 
 sim = SimulationJob(
     gamma,
-    Normal(0, theta_std),
-    lambda x: int(
-        (abs(x - w_[0]) <= rho) | (abs(x - w_[1]) <= rho)
-    ),  # Indicator function. Explanation p. 230 (bounded confidence)
-    lambda x: (1 - x ** 2) ** exponent,  # P&T p. 248
-    1000,
-    1000
+    theta_std,
+    lambda g, w: (1 - g) / (1 + abs(w)),
+    lambda w: 1,
+    lambda w: (1 - w ** 2),  # P&T p. 241
+    100,
+    10000,
 )
 
 sim.run()
 
 result_df = pd.Series(sim.result, name="opinion")
 
-fig = px.histogram(result_df, x="opinion", nbins=200, histnorm="density")
-fig.show()
+# computing histogram data
+
+counts, bins = np.histogram(result_df, bins=np.linspace(-1, 1, 200))
+bins = 0.5 * (bins[:-1] + bins[1:])
+
+# Scaling of reference solution
+mean_sim_result = np.mean(counts)
+mean_ref_result = np.mean(reference["g_inf(w)"])
+reference["g_inf(w)"] = (mean_sim_result / mean_ref_result) * reference["g_inf(w)"]
+
+# Generating figure
+plt.ion()
+
+fig = plt.figure()
+plt.bar(x=bins, height=counts, width=2 / len(bins))
+plt.plot(reference["w"], reference["g_inf(w)"], "r")
+cfm = plt.get_current_fig_manager()
+cfm.window.activateWindow()
+cfm.window.raise_()
+plt.show(block=True)
