48aa2bed5eb48661b8f885e1704c37489fdfa337
w_cossey <willem.cossey@student.kuleuven.be>
2021-12-09 20:59:27 +0100

    Add plotting routine for posterior sampling result

--- src/helper/ExperimentVisualizer.py
+++ src/helper/ExperimentVisualizer.py
@@ -31,3 +31,31 @@ class ExperimentVisualizer:
         plt.xlabel("Opinion []")
         plt.ylabel("Count []")
         plt.show(block=True)
+
+    @staticmethod
+    def from_samples_file(filename, burn_in, lmb, m):
+        num_burn_in = burn_in
+        try:
+            filepath = pth.join("../experiment-data", filename)
+            experiment_data = np.load(filepath)
+            f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+            counts, bins = np.histogram(
+                experiment_data["lmb"][num_burn_in:], bins=np.linspace(0, 2 * lmb, 200)
+            )
+            bins = 0.5 * (bins[:-1] + bins[1:])
+            ax1.bar(x=bins, height=counts, width=2 / len(bins))
+            ax1.set_xlabel("lambda []")
+            ax1.set_ylabel("Count []")
+            counts, bins = np.histogram(
+                experiment_data["m"][num_burn_in:], bins=np.linspace(-1, 1, 200)
+            )
+            bins = 0.5 * (bins[:-1] + bins[1:])
+            ax2.bar(x=bins, height=counts, width=2 / len(bins))
+            ax2.set_xlabel("mean opinion []")
+            ax2.set_ylabel("Count []")
+            f.suptitle(
+                f"Estimated parameters for data with underlying lambda = {lmb} and m = {m}"
+            )
+
+        except IOError:
+            print(IOError)
