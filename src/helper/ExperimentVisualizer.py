b44a0b3158da041f200adecace33caead570cf09
w_cossey <willem.cossey@student.kuleuven.be>
2021-12-14 15:19:00 +0100

    Add plot as time series

--- src/helper/ExperimentVisualizer.py
+++ src/helper/ExperimentVisualizer.py
@@ -33,29 +33,61 @@ class ExperimentVisualizer:
         plt.show(block=True)
 
     @staticmethod
-    def from_samples_file(filename, burn_in, lmb, m):
+    def from_samples_file(filename, burn_in, lmb, m, mode="hist"):
         num_burn_in = burn_in
         try:
-            filepath = pth.join("../experiment-data", filename)
+            filepath = filename
+            print(f"Plotting parameter estimates from {filepath}")
             experiment_data = np.load(filepath)
-            f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-            counts, bins = np.histogram(
-                experiment_data["lmb"][num_burn_in:], bins=np.linspace(0, 2 * lmb, 200)
-            )
-            bins = 0.5 * (bins[:-1] + bins[1:])
-            ax1.bar(x=bins, height=counts, width=2 / len(bins))
-            ax1.set_xlabel("lambda []")
-            ax1.set_ylabel("Count []")
-            counts, bins = np.histogram(
-                experiment_data["m"][num_burn_in:], bins=np.linspace(-1, 1, 200)
-            )
-            bins = 0.5 * (bins[:-1] + bins[1:])
-            ax2.bar(x=bins, height=counts, width=2 / len(bins))
-            ax2.set_xlabel("mean opinion []")
-            ax2.set_ylabel("Count []")
-            f.suptitle(
-                f"Estimated parameters for data with underlying lambda = {lmb} and m = {m}"
-            )
+            if mode == "hist":
+                f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+                counts, bins = np.histogram(
+                    experiment_data["lmb"][num_burn_in:],
+                    bins=np.linspace(0, 2 * lmb, 200),
+                )
+                bins = 0.5 * (bins[:-1] + bins[1:])
+                ax1.bar(x=bins, height=counts, width=2 / len(bins))
+                ax1.set_xlabel("lambda []")
+                ax1.set_ylabel("Count []")
+                counts, bins = np.histogram(
+                    experiment_data["m"][num_burn_in:], bins=np.linspace(-1, 1, 200)
+                )
+                bins = 0.5 * (bins[:-1] + bins[1:])
+                ax2.bar(x=bins, height=counts, width=2 / len(bins))
+                ax2.set_xlabel("mean opinion []")
+                ax2.set_ylabel("Count []")
+                f.suptitle(
+                    f"Estimated parameters for data with underlying lambda = {lmb} and m = {m} \n ensemble avgs: lambda = {np.mean(experiment_data['lmb'][num_burn_in:]): .2f}, m = {np.mean(experiment_data['m'][num_burn_in:]): .2f}"
+                )
+                plt.show(block=True)
+            if mode == "series":
+                f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+                ax1.plot(
+                    range(0, num_burn_in),
+                    experiment_data["lmb"][0:num_burn_in],
+                    "r",
+                    label="burn-in",
+                )
+                ax1.plot(
+                    range(num_burn_in, len(experiment_data["lmb"])),
+                    experiment_data["lmb"][num_burn_in:],
+                )
+                ax1.set_xlabel("index []")
+                ax1.set_ylabel("Lambda []")
+                ax2.plot(
+                    range(0, num_burn_in), experiment_data["m"][0:num_burn_in], "r"
+                )
+                ax2.plot(
+                    range(num_burn_in, len(experiment_data["m"])),
+                    experiment_data["m"][num_burn_in:],
+                )
+                ax2.set_xlabel("index []")
+                ax2.set_ylabel("mean opinion []")
+                f.suptitle(
+                    f"Series of samples for data with underlying lambda = {lmb} and m = {m} \n ensemble avgs: lambda = {np.mean(experiment_data['lmb'][num_burn_in:]): .2f}, m = {np.mean(experiment_data['m'][num_burn_in:]): .2f}"
+                )
+                ax1.legend()
+                plt.show(block=True)
 
         except IOError:
-            print(IOError)
+            print(f"Not found: {filename}")
