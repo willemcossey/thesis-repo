4d286ff83f8bc4731c72f925c9d5240312a95177
willemcossey <43749418+willemcossey@users.noreply.github.com>
2022-05-17 10:39:15 +0200

    Adjust evaluation points of analytical solution and change error measure

--- src/experiment-12-simulation-noise.py
+++ src/experiment-12-simulation-noise.py
@@ -37,7 +37,11 @@ y = torch.from_numpy(
     data.get_outputs(end=n_samples, otype="aggregated", silent=False)
 ).to(torch.float)
 
-p = np.linspace(-1.0, 1.0, num=n_buckets)
+
+h = 2 / n_buckets
+centers = [-1 + h / 2 + i * h for i in range(n_buckets)]
+p = centers + [-1, 1]
+p.sort()
 y_exact = [inv_dist(s, x[0, 1], x[0, 0]) for s in p]
 print(np.array(y_exact).sum())
 y_exact = y_exact / (np.array(y_exact).sum())
@@ -47,10 +51,10 @@ y_exact = y_exact / (np.array(y_exact).sum())
 y_mean = torch.mean(y, dim=0)
 
 plt.figure()
-ax = plt.plot(range(1, n_buckets + 1), y_mean, color="red")
-plt.plot(range(1, n_buckets + 1), y_exact, color="green")
+ax = plt.plot(centers, y_mean, color="red")
+plt.plot(p, y_exact, color="green")
 
-plt.violinplot(torch.transpose(y, 0, 1), vert=True)
+plt.violinplot(torch.transpose(y, 0, 1), positions=centers, vert=True, widths=0.05)
 
 # plt.boxplot(torch.transpose(y,0,1),vert=True,showmeans=True)
 
@@ -73,6 +77,7 @@ plt.title(
 
 RMSE = torch.sqrt(torch.mean((y - y_mean) ** 2))
 MAE = torch.mean(abs(y - y_mean))
+MRAE = torch.mean(abs(y - y_mean)) / torch.mean(abs(y))
 y_std = torch.median(torch.std(y, dim=0))
 RAE = torch.mean(abs(y - y_mean) / (y_mean)) * 100
 
@@ -80,5 +85,6 @@ RAE = torch.mean(abs(y - y_mean) / (y_mean)) * 100
 # print(f"mean: {y_mean}")
 print(f"median std: {y_std**2}")
 print(f"MAE: {MAE}")
+print(f"MRAE: {MRAE}")
 print(f"RMSE: {RMSE}")
 print(f"mean relative error: {RAE:.2f}%")
