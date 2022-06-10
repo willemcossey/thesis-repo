28f20bd956ea79e128e259b7080d6cef9ddc57e5
willemcossey <43749418+willemcossey@users.noreply.github.com>
2022-06-10 03:03:14 +0200

    Add block option and latest style to ExperimentVisualizer

--- src/helper/ExperimentVisualizer.py
+++ src/helper/ExperimentVisualizer.py
@@ -1,7 +1,10 @@
 import os.path as pth
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from os import path
 
+mpl.style.use(path.join("..", "src", "grayscale_adjusted.mplstyle"))
 
 # load an experiment from experiment-data
 
@@ -21,18 +24,20 @@ class ExperimentVisualizer:
             print(f"Not found: {filepath}")
 
     @staticmethod
-    def from_array(arr):
+    def from_array(arr, block=True):
         plt.figure()
         counts, bins = np.histogram(arr, bins=np.linspace(-1, 1, 200))
         bins = 0.5 * (bins[:-1] + bins[1:])
         plt.bar(x=bins, height=counts, width=2 / len(bins))
         plt.xlabel("Opinion []")
         plt.ylabel("Count []")
-        plt.show(block=True)
+        plt.show(block=block)
         return plt
 
     @staticmethod
-    def from_samples_file(filename, burn_in, lmb, m, mode="hist"):
+    def from_samples_file(
+        filename, burn_in, lmb, m, mode="hist", block=True, title=True
+    ):
         num_burn_in = burn_in
         try:
             filepath = filename
@@ -48,45 +53,51 @@ class ExperimentVisualizer:
                         200,
                     ),
                 )
-                ax1.set_xlabel("lambda []")
-                ax1.set_ylabel("Count []")
+                ax1.set_xlabel("$\lambda$")
+                ax1.set_ylabel("Count")
                 ax2.hist(
                     experiment_data["m"][num_burn_in:], bins=np.linspace(-1, 1, 200)
                 )
-                ax2.set_xlabel("mean opinion []")
-                ax2.set_ylabel("Count []")
-                f.suptitle(
-                    f"Estimated parameters for data with underlying lambda = {lmb} and m = {m} \n ensemble avgs: lambda = {np.mean(experiment_data['lmb'][num_burn_in:]): .2f}, m = {np.mean(experiment_data['m'][num_burn_in:]): .2f}"
-                )
-                plt.show(block=True)
+                ax2.set_xlabel("$m$")
+                ax2.set_ylabel("Count")
+                if title:
+                    f.suptitle(
+                        f"Ensemble averages: $\lambda$ = {np.mean(experiment_data['lmb'][num_burn_in:]): .2f}, $m$ = {np.mean(experiment_data['m'][num_burn_in:]): .2f}"
+                    )
+                plt.show(block=block)
             if mode == "series":
                 f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-                ax1.plot(
-                    range(0, num_burn_in),
-                    experiment_data["lmb"][0:num_burn_in],
-                    "r",
-                    label="burn-in",
-                )
                 ax1.plot(
                     range(num_burn_in, len(experiment_data["lmb"])),
                     experiment_data["lmb"][num_burn_in:],
                 )
-                ax1.set_xlabel("index []")
-                ax1.set_ylabel("Lambda []")
+                ax1.plot(
+                    range(0, num_burn_in),
+                    experiment_data["lmb"][0:num_burn_in],
+                    "-.",
+                    label="burn-in",
+                )
+                ax1.set_xlabel("index")
+                ax1.set_ylabel("$\lambda$")
                 ax2.plot(
-                    range(0, num_burn_in), experiment_data["m"][0:num_burn_in], "r"
+                    range(num_burn_in, len(experiment_data["m"])),
+                    experiment_data["m"][num_burn_in:],
+                )
+                ax2.plot(
+                    range(0, num_burn_in), experiment_data["m"][0:num_burn_in], "-."
                 )
                 ax2.plot(
                     range(num_burn_in, len(experiment_data["m"])),
                     experiment_data["m"][num_burn_in:],
                 )
-                ax2.set_xlabel("index []")
-                ax2.set_ylabel("mean opinion []")
-                f.suptitle(
-                    f"Series of samples for data with underlying lambda = {lmb} and m = {m} \n ensemble avgs: lambda = {np.mean(experiment_data['lmb'][num_burn_in:]): .2f}, m = {np.mean(experiment_data['m'][num_burn_in:]): .2f}"
-                )
+                ax2.set_xlabel("index")
+                ax2.set_ylabel("$m$")
+                if title:
+                    f.suptitle(
+                        f"Ensemble averages: $\lambda$ = {np.mean(experiment_data['lmb'][num_burn_in:]): .2f}, $m$ = {np.mean(experiment_data['m'][num_burn_in:]): .2f}"
+                    )
                 ax1.legend()
-                plt.show(block=True)
+                plt.show(block=block)
 
         except IOError:
             print(f"Not found: {filename}")
